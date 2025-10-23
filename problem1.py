d1ac6ebbfc185d3925f8199f1b875d09e616a472
MichaelBCP <M.Ghobrial28@bcp.org>
2025-10-23 22:43:16 +0000

    problem 1

--- problem1.py
+++ problem1.py
@@ -1,3 +1,11 @@
 og_string = "hello"
 rev_string = og_string[::-1]
 print (rev_string)
+
+def reverse_string(string):
+   new_string = ""
+   charNum = len(string) - 1
+   while charNum >= 0:
+      new_string += (string[charNum])
+      charNum -= 1
+   return new_string
